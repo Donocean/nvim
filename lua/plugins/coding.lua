8ae31f23c02731578801d5cb8dd3f97b89cd8392
donocean <1184427366@qq.com>
2023-05-24 01:15:53 +0800

    fix luasnip bug

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -3,6 +3,7 @@ return {
     -- snippets
     {
         "L3MON4D3/LuaSnip",
+        event = "InsertEnter",
         build = (not jit.os:find("Windows"))
             and "echo -e 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp"
             or nil,
@@ -17,19 +18,19 @@ return {
             delete_check_events = "TextChanged",
         },
         -- stylua: ignore
-        keys = {
-            {
-                "<tab>",
-                function()
-                    return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
-                end,
-                expr = true,
-                silent = true,
-                mode = "i",
-            },
-            { "<tab>",   function() require("luasnip").jump(1) end,  mode = "s" },
-            { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
-        },
+        -- keys = {
+        --     {
+        --         "<tab>",
+        --         function()
+        --             return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+        --         end,
+        --         expr = true,
+        --         silent = true,
+        --         mode = "i",
+        --     },
+        --     { "<tab>",   function() require("luasnip").jump(1) end,  mode = "s" },
+        --     { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
+        -- },
     },
 
     -- auto completion
