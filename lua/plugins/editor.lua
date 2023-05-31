6afea81dd14d780546325f0f537c1caecd2292b5
donocean <1184427366@qq.com>
2023-06-01 00:16:15 +0800

    fix

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -278,21 +278,21 @@ return {
                 end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
             end
 
-            map("]]", "next")
-            map("[[", "prev")
+            map("]n", "next")
+            map("[n", "prev")
 
             -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
             vim.api.nvim_create_autocmd("FileType", {
                 callback = function()
                     local buffer = vim.api.nvim_get_current_buf()
-                    map("]]", "next", buffer)
-                    map("[[", "prev", buffer)
+                    map("]n", "next", buffer)
+                    map("[n", "prev", buffer)
                 end,
             })
         end,
         keys = {
-            { "]]", desc = "Next Reference" },
-            { "[[", desc = "Prev Reference" },
+            { "]n", desc = "Next Reference" },
+            { "[n", desc = "Prev Reference" },
         },
     },
 
