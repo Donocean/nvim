bbb9183caaab95b25edb0792c236e449ab3f8c08
donocean <1184427366@qq.com>
2023-06-14 23:05:42 +0800

    little changes

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -148,18 +148,17 @@ return {
         opts = function()
             local dashboard = require("alpha.themes.dashboard")
             local logo = [[
-      ██╗   ██╗   ██╗   ███╗   ███╗
-      ██║   ██║   ██║   ████╗ ████║
-      ██║   ██║   ██║   ██╔████╔██║
-      ╚██╗ ██╔╝   ██║   ██║╚██╔╝██║
-       ╚████╔╝    ██║   ██║ ╚═╝ ██║
-        ╚═══╝     ╚═╝   ╚═╝     ╚═╝
+     ██╗   ██╗   ██╗   ███╗   ███╗       
+     ██║   ██║   ██║   ████╗ ████║       
+     ██║   ██║   ██║   ██╔████╔██║       
+     ╚██╗ ██╔╝   ██║   ██║╚██╔╝██║       
+      ╚████╔╝    ██║   ██║ ╚═╝ ██║       
+       ╚═══╝     ╚═╝   ╚═╝     ╚═╝       
       ]]
 
             dashboard.section.header.val = vim.split(logo, "\n")
             dashboard.section.buttons.val = {
                 dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-                dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
                 dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
                 dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
                 dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
