750ec1da19f86ced7f491680d1f1e3893a0b91de
Donocean <1184427366@qq.com>
2023-07-04 23:33:10 +0800

    background transparent support

--- init.lua
+++ init.lua
@@ -9,4 +9,8 @@ require("config.autocmds")
 require("config.keymaps")
 
 -- load scheme(tokyonight & gruvbox)
-vim.cmd([[colorscheme gruvbox]])
+vim.cmd.colorscheme("gruvbox")
+
+-- background transparent
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
