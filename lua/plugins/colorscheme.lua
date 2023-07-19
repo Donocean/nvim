33808f76b753875a96f5d6e96ae432cb7eb048f3
Donocean <1184427366@qq.com>
2023-07-19 16:18:32 +0800

    update: small changs

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,16 +9,8 @@ return {
 
     --  everforest
     {
-        "neanias/everforest-nvim",
-        version = false,
+        "sainnhe/everforest",
         lazy = false,
-        -- make sure to load this before all the other start plugins, this is a file
         priority = 1000,
-        -- Optional; default configuration will be used if setup isn't called.
-        config = function()
-            require("everforest").setup({
-                -- Your config here
-            })
-        end,
     },
 }
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -32,7 +32,7 @@ return {
             -- add any global capabilities here
             capabilities = {},
             -- Automatically format on save
-            autoformat = true,
+            autoformat = false,
             -- disable diagnostics
             diagnostics_enabled = false,
 
@@ -211,23 +211,23 @@ return {
         end,
     },
     -- formatters
-    {
-        "jose-elias-alvarez/null-ls.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = { "mason.nvim" },
-        opts = function()
-            local nls = require("null-ls")
-            return {
-                root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-                sources = {
-                    nls.builtins.formatting.stylua,
-                    nls.builtins.formatting.shfmt,
-                    nls.builtins.formatting.clang_format.with({
-                        extra_args = {"--style=Microsoft"},
-                    }),
-                    -- nls.builtins.diagnostics.flake8,
-                },
-            }
-        end,
-    },
+    -- {
+    --     "jose-elias-alvarez/null-ls.nvim",
+    --     event = { "BufReadPre", "BufNewFile" },
+    --     dependencies = { "mason.nvim" },
+    --     opts = function()
+    --         local nls = require("null-ls")
+    --         return {
+    --             root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+    --             sources = {
+    --                 nls.builtins.formatting.stylua,
+    --                 nls.builtins.formatting.shfmt,
+    --                 nls.builtins.formatting.clang_format.with({
+    --                     extra_args = {"--style=Microsoft"},
+    --                 }),
+    --                 -- nls.builtins.diagnostics.flake8,
+    --             },
+    --         }
+    --     end,
+    -- },
 }
