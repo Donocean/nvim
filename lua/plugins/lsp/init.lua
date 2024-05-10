294ea7bef5b9e5fc14d79a2e62fcce350a675458
Donocean <1184427366@qq.com>
2024-05-10 11:18:29 +0800

    remove: auto include header

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -51,6 +51,11 @@ return {
                     },
                 },
             },
+            clangd = {
+                cmd = {
+                    "--header-insertion=never",
+                }
+            },
             -- you can do any additional lsp server setup here
             -- return true if you don't want this server to be setup with lspconfig
             ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
