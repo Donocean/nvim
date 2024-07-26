f8375996b596eb62f42543e8df2e40dc05ac9da6
Donocean <1184427366@qq.com>
2024-07-26 09:39:37 +0800

    new clang format

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -17,9 +17,6 @@ return {
     end,
     -- stylua: ignore
     keys = {
-        { "<tab>", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
-        { "<S-tab>", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-        { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-        { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        { "q", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
     },
 }
