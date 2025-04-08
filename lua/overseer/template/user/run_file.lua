fb8bae5691fd2541cea6ea2b7c9a5f2ceb72b4bc
donocean <1184427366@qq.com>
2025-04-08 17:04:02 +0800

    update clang setttings

--- lua/overseer/template/user/run_file.lua
+++ lua/overseer/template/user/run_file.lua
@@ -1,24 +1,19 @@
 return {
     name = "run file(c/c++/py)",
     builder = function()
-        local ft = vim.bo.filetype
-
         local lang_cmd = {}
         local lang_args = {}
-
-        local function directory_exists(dir)
-            local stat = vim.loop.fs_stat(dir)
-            return stat and stat.type == 'directory'
-        end
+        local ft = vim.bo.filetype
 
         local file = vim.api.nvim_buf_get_name(0)  -- get full path of current file
         if ft == "c" or ft == "cpp" then
+            local Util = require("util")
             local current_path = vim.fn.fnamemodify(file, ":h")  -- no file name, only need abs path
-            local build_path = current_path .. "/build"
+            local build_root = Util.find_path_on_pattern(current_path, "/build")
 
-            if directory_exists(build_path) then
+            if build_root ~= nil then
                 -- vim.fn.input("Path to executable: ", build_path .. "/", "file")
-                lang_cmd = build_path .. "/demo"
+                lang_cmd = build_root .. "/build/demo"
             else
                 -- vim.fn.input("Path to executable: ", current_path .. "/", "file")
                 lang_cmd = current_path .. "/demo"
