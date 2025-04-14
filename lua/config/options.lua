63a06263961b3dbb7dd733d1242a13d1cb6ca9ab
donocean <1184427366@qq.com>
2025-04-15 02:00:40 +0800

    use osc52 to copy ssh content

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -75,3 +75,17 @@ vim.filetype.add({
         urdf = 'xml',
     }
 })
+
+if os.getenv("SSH_TTY") then
+    vim.g.clipboard = {
+        name = 'OSC 52',
+        copy = {
+            ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+            ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+        },
+        paste = {
+            ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+            ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+        },
+    }
+end
